33b287f671992010ddeef1e8d275fbfeae37d9a9
lylejover <lylejover2yahoo.com>
2018-01-26 12:36:23 -0700

    stuff floating around

--- WorldTracking/ViewController.swift
+++ WorldTracking/ViewController.swift
@@ -17,14 +17,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
         
     }
 
     @IBAction func addBtnPressed(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
-        node.position = SCNVector3(0,0,-0.3)
+        //node.position = SCNVector3(0,0,-0.3)
+        let x = randomNumber(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumber(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumber(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(x, y, z)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
@@ -38,6 +44,10 @@ class ViewController: UIViewController {
             node.removeFromParentNode()
         }
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func randomNumber(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
     
 }
