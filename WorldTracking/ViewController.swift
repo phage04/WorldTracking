46307f6415c14a48cc85b22145aff8609876cc7e
lylejover <lylejover2yahoo.com>
2018-01-26 22:41:53 -0700

    rotations

--- WorldTracking/ViewController.swift
+++ WorldTracking/ViewController.swift
@@ -27,20 +27,11 @@ class ViewController: UIViewController {
         let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
         boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         let node = SCNNode()
-        // node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
-        //        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
-        //        let path = UIBezierPath()
-        //        path.move(to: CGPoint(x: 0, y: 0))
-        //        path.addLine(to: CGPoint(x:0, y: 0.2))
-        //        path.addLine(to: CGPoint(x: 0.2, y:0.3))
-        //        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
-        //        path.addLine(to: CGPoint(x: 0.4, y: 0))
-        //        let shape = SCNShape(path: path, extrusionDepth: 0.2)
-        //        node.geometry = shape
         node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
         node.geometry?.firstMaterial?.specular.contents = UIColor.orange
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
         node.position = SCNVector3(0.2,0.3,-0.2)
+        node.eulerAngles = SCNVector3(Float(180.degreesToRadians),0,0)
         boxNode.position = SCNVector3(0, -0.05, 0)
         doorNode.position = SCNVector3(0,-0.02,0.053)
         self.sceneView.scene.rootNode.addChildNode(node)
@@ -65,5 +56,11 @@ class ViewController: UIViewController {
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
     
+
+    
 }
 
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
